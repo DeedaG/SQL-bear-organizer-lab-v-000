f79c28d287bc5c93045c1be5fdf29e1621d2444c
Christia T <christiatidwell@bellsouth.net>
2018-08-08 21:12:38 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, M, "Brown", "Sweet", 1);
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, M, "Red", "Hyper", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, F, "White", "Quiet", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Seargent Brown", 10, "Grey", "Tough", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
